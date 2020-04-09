2fd96d191f39c530e2051907107101bc7c3c59ea
Aakarshit Agarwal <aakarshit.agarwal@gmail.com>
2020-04-10 03:16:01 +0530

    Python Upgrade

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ class MyClient(discord.Client):
         print('Logged on as', self.user)
 
     async def on_message(self, message):
-        # don't respond to ourselves
         print(message.channel, message.author, message.author.name, message.content)
         if message.author == self.user:
-            await message.channel.send('Hey You')
+            return 
 
         if message.content == 'ping':
             await message.channel.send('pong')
 
 client = MyClient()
 client.run('Njk3MTE5NTQ2NzY2Nzg2NjEw.XoypXg.l8PDKKsqeaK_uVmPnomRn5psNVE')
+
